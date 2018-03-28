c3dcb8c176030739a3d41865047e6c2e89692a71
rodrigodjodi <rodrigo.djodi@gmail.com>
2018-03-28 19:00:05 -0300

    Inclusao de panorama de exemplo.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,33 @@
 <template>
   <div class="home">
-    <img src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <button @click="scene = 'padrao'">Padrão</button>
+    <button @click="scene = 'classico'">Clássico</button>
+    <button @click="scene = 'contemporaneo'">Contemporâneo</button>
+    <krpano :xml="'tour.xml'"
+      :lazy-load="true"
+      style="width:100vmin;height:70vmin"
+      @panoCreated="init"
+      :scene="scene"
+    />
+
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
+import Krpano from "vue-krpano";
 export default {
   name: "home",
-  components: {
-    HelloWorld
+  components: { Krpano },
+  data() {
+    return {
+      scene: 3
+    };
+  },
+  methods: {
+    init() {
+      console.log("initialized");
+    }
   }
 };
 </script>
---
+++ vue.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  configureWebpack: {
+    //this is because the krpano component needs the full build of vue, with the compiler
+    resolve: {
+      alias: {
+        vue$: "vue/dist/vue.esm.js"
+      }
+    }
+  }
+};
