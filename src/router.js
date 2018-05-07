586629aecbf5c84fb4a3f535b2ab4160576e12b9
rodrigodjodi <rodrigo.djodi@gmail.com>
2018-05-07 17:00:36 -0300

    changed error position for reading

--- src/router.js
+++ src/router.js
@@ -1,10 +1,10 @@
 import Vue from "vue";
 import Router from "vue-router";
 const Home = () => import("./views/Home.vue");
+const Erro = () => import("./views/Erro.vue");
 const Admin = () => import("./views/Admin.vue");
 const Login = () => import("./views/Login.vue");
 const FinishLogin = () => import("./views/FinishLogin.vue");
-const Erro = () => import("./views/Erro.vue");
 import { auth } from "./firebase";
 import store from "./store";
 Vue.use(Router);
