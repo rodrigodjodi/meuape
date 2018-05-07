410540d8d633f2a0324eab927e0c02d742e55d48
rodrigodjodi <rodrigo.djodi@gmail.com>
2018-05-07 12:00:13 -0300

    removed unused dependencies

--- src/main.js
+++ src/main.js
@@ -3,8 +3,6 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 //import "./registerServiceWorker";
-import ToggleButton from "vue-js-toggle-button";
-Vue.use(ToggleButton);
 Vue.config.productionTip = false;
 new Vue({
   router,
