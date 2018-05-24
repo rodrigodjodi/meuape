36712ac57916ced36e2d719b06d1791e93d7e6c5
rodrigodjodi <rodrigo.djodi@gmail.com>
2018-05-24 16:31:28 -0300

    optimize firebase imports

--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,7 @@
-import { initializeApp } from "firebase";
-const app = initializeApp({
+import firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/database";
+const app = firebase.initializeApp({
   apiKey: "AIzaSyA2Iimxi5jfoxy20NLZ9L1wrXOnt1HXJyY",
   authDomain: "sistema-pmnt.firebaseapp.com",
   databaseURL: "https://sistema-pmnt.firebaseio.com",
